01053e5d1cc5039c5d7b3b71790c50b2d86e3dae
Jonathan Kovacs <jdk@jdk.ca>
2015-05-29 17:55:21 -0400

    Added aot optimizations to upgrade script.

--- develop/sonarr-update.sh
+++ develop/sonarr-update.sh
@@ -6,5 +6,8 @@ echo "updating sonarr"
 rm -Rfv /opt/NzbDrone/*
 mv $2/NzbDrone/* /opt/NzbDrone/
 
+mono --aot /opt/NzbDrone/NzbDrone.exe \
+	&& for i in /opt/NzbDrone/*.dll; do mono --aot $i; done
+
 echo "sending term to sonarr, configure docker to automatically restart this container"
 kill $1
